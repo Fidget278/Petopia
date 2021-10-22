171b520347e556e28365256cea4aff613153eb02
Gapus <tjdwnd751@naver.com>
2021-10-22 18:11:46 +0900

    [기능추가] 프로필 Dao, Service 기능 추가

--- src/controller/member/IntroCommand.java
+++ src/controller/member/IntroCommand.java
@@ -2,16 +2,27 @@ package controller.member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.ActionForward;
 import controller.Command;
+import model.member.MemberDao;
+import model.member.MemberService;
+import model.member.MemberVo;
 
 public class IntroCommand implements Command {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
+		HttpSession session = request.getSession();
+		MemberVo memberVo = (MemberVo) session.getAttribute("user");
+
+		MemberVo memberProfile = MemberService.getInstance().retreiveMemberProfile(memberVo.getNo());
+
+		session.setAttribute("user", memberProfile);
+
 		return new ActionForward("/homeIndex.jsp", false);
 	}
-	
+
 }
