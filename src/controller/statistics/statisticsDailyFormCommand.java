d9e85eb0fdaa70327be5a0d9358f0f001ee3b7d0
Gapus <tjdwnd751@naver.com>
2021-10-25 16:47:38 +0900

    [기능추가] 통계 추가, 수정 Dao 추가 / css footer 이상하게 나오는것 수정

--- src/controller/statistics/statisticsDailyFormCommand.java
+++ src/controller/statistics/statisticsDailyFormCommand.java
@@ -1,6 +1,8 @@
 package controller.statistics;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +21,23 @@ public class statisticsDailyFormCommand implements Command {
 
 		ArrayList<StatisticsVo> statisticsDailyList = statisticsService.retriveDailyData();
 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar c1 = Calendar.getInstance();
+		String strToday = sdf.format(c1.getTime());
+
+		System.out.println("strToday : " + strToday);
+		System.out.println("getDailyDate : " + statisticsDailyList.get(0).getDailyDate());
+
+		if (!strToday.equals(statisticsDailyList.get(0).getDailyDate())) {
+			System.out.println("동일하지 않아서 인서트 실행");
+			statisticsService.registerDailyData(); 
+		}else {
+			System.out.println("동일해서 업데이트 실행");
+			statisticsService.modifyDailyData();
+		}
+		statisticsDailyList = statisticsService.retriveDailyData();
+		
+		
 		request.setAttribute("dailyList", statisticsDailyList);
 
 		request.setAttribute("viewheader", "viewManagerHeader");
